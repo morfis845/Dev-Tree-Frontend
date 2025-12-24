9722fb5f6c133da29d2abd814a4886a76921ddaa
Morfis <manciajose1208@gmail.com>
2025-12-24 13:18:27 -0600

    feat: agregar navegación y vistas de administración con enlaces a perfil y LinkTree

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginView from "@/views/loginView";
 import RegisterView from "@/views/registerView";
 import AuthLayout from "@/layouts/AuthLayout";
+import AppLayout from "./layouts/AppLayout";
+import LinkTreeView from "./views/LinkTreeView";
+import ProfileView from "./views/ProfileView";
 
 export default function Router() {
   return (
@@ -10,6 +13,10 @@ export default function Router() {
         <Route element={<AuthLayout />}>
           <Route path="/auth/login" element={<LoginView />} />
           <Route path="/auth/register" element={<RegisterView />} />
+        </Route>
+        <Route element={<AppLayout />} path="/admin">
+          <Route index={true} element={<LinkTreeView />} />
+          <Route path="profile" element={<ProfileView />} />
         </Route>
       </Routes>
     </BrowserRouter>
