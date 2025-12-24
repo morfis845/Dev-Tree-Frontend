962ebfb2f1f4835ca2d4aa3ad5166652251feca6
Morfis <manciajose1208@gmail.com>
2025-12-24 11:27:07 -0600

    feat: implementar vista de inicio de sesión con validación de formulario y manejo de errores

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,3 +8,7 @@ export type RegisterUser = Pick<User, "name" | "email" | "handle"> & {
   password: string;
   password_confirmation: string;
 };
+
+export type LoginUser = Pick<User, "email"> & {
+  password: string;
+};
