91c784a0e0bc61ced8eb3c4899f4e831127e9051
Morfis <manciajose1208@gmail.com>
2025-12-25 11:18:05 -0600

    feat: agregar funcionalidad para manejar enlaces en DevTree y optimizar estilos

--- src/components/DevTreeInput.tsx
+++ src/components/DevTreeInput.tsx
@@ -1,17 +1,47 @@
 import type { DevTreeLink } from "@/types";
+import { classNames } from "@/utils/index";
+import { Switch } from "@headlessui/react";
 
 type LinkProps = {
   link: DevTreeLink;
+  handleUrlChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  name: string;
 };
 
-export default function DevTreeInput({ link }: LinkProps) {
+export default function DevTreeInput({
+  link,
+  handleUrlChange,
+  name,
+}: LinkProps) {
   return (
     <div className="bg-white shadow-sm p-5 flex items-center gap-5">
       <div
         className="w-12 h-12 bg-cover"
         style={{ backgroundImage: `url(/social/icon_${link.name}.svg)` }}
       ></div>
-      <input type="text" className="flex-1 border border-gray-300 rounded-lg" />
+      <input
+        type="text"
+        className="flex-1 border border-gray-300 rounded-lg"
+        value={link.url}
+        onChange={handleUrlChange}
+        name={name}
+      />
+      <Switch
+        checked={link.enabled}
+        onChange={() => {}}
+        className={classNames(
+          link.enabled ? "bg-blue-500" : "bg-gray-200",
+          "relative inline-flex h-6 w-11 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+        )}
+      >
+        <span
+          aria-hidden="true"
+          className={classNames(
+            link.enabled ? "translate-x-5" : "translate-x-0",
+            "pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
+          )}
+        />
+      </Switch>
     </div>
   );
 }
--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -5,11 +5,20 @@ import { useState } from "react";
 export default function LinkTreeView() {
   const [devTreeLinks, setDevTreeLinks] = useState(social);
 
+  const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.name, e.target.value);
+  };
+
   return (
     <>
       <div className="flex flex-1 flex-col gap-5">
         {devTreeLinks.map((link) => (
-          <DevTreeInput key={link.name} link={link} />
+          <DevTreeInput
+            key={link.name}
+            link={link}
+            handleUrlChange={handleUrlChange}
+            name={link.name}
+          />
         ))}
       </div>
     </>
