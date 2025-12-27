de6457967839886c06a82a0ce0867b9d83a1ab03
Morfis <manciajose1208@gmail.com>
2025-12-27 17:58:50 -0600

    feat: agregar componente SearchForm; implementar lógica para búsqueda de usuarios por handle

---
+++ src/components/SearchForm.tsx
@@ -0,0 +1,47 @@
+import ErrorMessage from "./ErrorMessage";
+import slugify from "react-slugify";
+import { useForm } from "react-hook-form";
+
+export default function SearchForm() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      handle: "",
+    },
+  });
+
+  const handle = watch("handle");
+  const handleSearch = () => {
+    const slug = slugify(handle);
+    console.log("Searching for handle:", slug);
+  };
+  return (
+    <form onSubmit={handleSubmit(handleSearch)} className="space-y-5">
+      <div className="relative flex items-center  bg-white  px-2">
+        <label htmlFor="handle">devtree.com/</label>
+        <input
+          type="text"
+          id="handle"
+          className="border-none bg-transparent p-2 focus:ring-0 flex-1"
+          placeholder="elonmusk, zuck, jeffbezos"
+          {...register("handle", {
+            required: "Un Nombre de Usuario es obligatorio",
+          })}
+        />
+      </div>
+      {errors.handle && <ErrorMessage>{errors.handle.message}</ErrorMessage>}
+
+      <div className="mt-10"></div>
+
+      <input
+        type="submit"
+        className="bg-cyan-400 p-3 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
+        value="Obtener mi DevTree"
+      />
+    </form>
+  );
+}
--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,10 +1,11 @@
 import Header from "@/components/Header";
+import SearchForm from "@/components/SearchForm";
 
 export default function HomeView() {
   return (
     <>
       <Header />
-      <main className="bg-gray-100 py-10 min-h-screen">
+      <main className="bg-gray-100 py-10 min-h-screen lg:bg-[url('../../public/bg.svg')] bg-no-repeat bg-top-right bg-size-[50%]">
         <div className="max-w-5xl mx-auto mt-10">
           <div className="lg:w-1/2 px-10 lg:p-0 space-y-6">
             <h1 className="text-6xl font-black">
@@ -16,6 +17,7 @@ export default function HomeView() {
               comparte tu perfil de TikTok, Facebook, Instagram, Youtube,
               Github, y más.
             </p>
+            <SearchForm />
           </div>
         </div>
       </main>
