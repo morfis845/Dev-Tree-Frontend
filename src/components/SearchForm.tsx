d219c8d4320eebf44b40dbce1176d67231289161
Morfis <manciajose1208@gmail.com>
2025-12-27 19:31:00 -0600

    feat: agregar lógica de búsqueda por handle; implementar manejo de estado y mensajes en SearchForm

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,6 +1,8 @@
 import ErrorMessage from "./ErrorMessage";
 import slugify from "react-slugify";
 import { useForm } from "react-hook-form";
+import { useMutation } from "@tanstack/react-query";
+import { searchByHandle } from "@/api/DevTreeApi";
 
 export default function SearchForm() {
   const {
@@ -14,11 +16,16 @@ export default function SearchForm() {
     },
   });
 
+  const mutation = useMutation({
+    mutationFn: searchByHandle,
+  });
+  const { data, isPending, isError, error } = mutation;
   const handle = watch("handle");
   const handleSearch = () => {
     const slug = slugify(handle);
-    console.log("Searching for handle:", slug);
+    mutation.mutate(slug);
   };
+
   return (
     <form onSubmit={handleSubmit(handleSearch)} className="space-y-5">
       <div className="relative flex items-center  bg-white  px-2">
@@ -35,7 +42,15 @@ export default function SearchForm() {
       </div>
       {errors.handle && <ErrorMessage>{errors.handle.message}</ErrorMessage>}
 
-      <div className="mt-10"></div>
+      <div className="mt-10">
+        {isPending && <p className="text-center">Buscando...</p>}
+        {isError && <p className="text-center text-red-500">{error.message}</p>}
+        {data && (
+          <p className="text-center text-cyan-600">
+            El usuario {data[0]} esta disponible
+          </p>
+        )}
+      </div>
 
       <input
         type="submit"
