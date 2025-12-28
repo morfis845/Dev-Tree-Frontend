2fe92c1189ae4eebcdc1769c4079b8d0801caf47
Morfis <manciajose1208@gmail.com>
2025-12-27 20:48:47 -0600

    feat: eliminar importación innecesaria de Navigate en LoginView; agregar archivo de redirección

--- src/views/loginView.tsx
+++ src/views/loginView.tsx
@@ -1,4 +1,4 @@
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import ErrorMessage from "@/components/ErrorMessage";
 import type { LoginUser } from "@/types";
