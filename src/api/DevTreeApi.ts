5ef22b6c485f5892020f884bd6f1b54dbe297bc4
Morfis <manciajose1208@gmail.com>
2025-12-27 19:19:13 -0600

    feat: agregar función searchByHandle; implementar lógica para búsqueda de usuarios por handle

--- src/api/DevTreeApi.ts
+++ src/api/DevTreeApi.ts
@@ -63,3 +63,15 @@ export async function getUserByHandler(handle: string) {
     throw error;
   }
 }
+
+export async function searchByHandle(handle: string) {
+  try {
+    const { data } = await api.post<String>("/search", { handle });
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.message);
+    }
+    throw error;
+  }
+}
